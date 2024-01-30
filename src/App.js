9d46ec1bfd02e9046431491e8771faef97ddba56
Kimani-Dominic <muiruridominic001@gmail.com>
2024-01-30 14:03:47 +0300

    login page

--- src/App.js
+++ src/App.js
@@ -8,12 +8,19 @@ import UserDashboard from './UserDashboard';
 //UseReducer and Custom hooks components
 import Counter from './Counter';
 import useFetch from './useFetch';
+import LoginForm from './components/Login';
+//Router
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+
 
 function App() {
   const user = { name: 'Kimani Muiruri', email: 'kimanimuiruri@gmail.com' };
   //custom hook
   const { data, loading } = useFetch('https://jsonplaceholder.typicode.com/todos/1');
   return (
+    <Router>
     <div className="App">
       <h1>React Prop-Drilling Demo</h1>
       <ParentComponent />
@@ -26,9 +33,14 @@ function App() {
       <h1>State Management Demo</h1>
       <Counter />
       {loading ? <p>Loading...</p> : <p>Title: {data.title}</p>}
-      
+      <Link to="/login">Go to Login</Link>
     </div>
-  );
+    <Switch>
+      <Route path="/" component={App} />
+      <Route path="/login" component={LoginForm} />
+    </Switch>
+  </Router>
+);
 }
 
 export default App;
---
+++ src/themes/theme.js
@@ -0,0 +1,13 @@
+
+import { createTheme } from '@mui/material/styles';
+import { deepPurple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: deepPurple[500],
+    },
+  },
+});
+
+export default theme;
